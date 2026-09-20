b8e7ef1277b941496151cbc356b881cd161afc18
Fadel <fjachuf@gmail.com>
2026-09-20 18:59:46 -0300

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/m3ui-vue-docs/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
